dac26c0fecebfbd755cfebdfae758e4618be2172
Frédéric Sagnes <fred@teapotapps.com>
2013-04-18 11:19:23 +0200

    Add app icons, add iPad compatibility

---
+++ Resources/download_assets.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+
+if [ ! -f Icon.png ]
+then
+  rm -f Logo.png Icon.png Icon@2x.png Icon-72.png Icon-72@2x.png
+  curl -s -L -o Logo.png "https://api.twitter.com/1/users/profile_image?screen_name=nytimesbits&size=original"
+  sips -Z 57 Logo.png --out Icon.png
+  sips -Z 114 Logo.png --out Icon@2x.png
+  sips -Z 72 Logo.png --out Icon-72.png
+  sips -Z 144 Logo.png --out Icon-72@2x.png
+  rm Logo.png
+fi
